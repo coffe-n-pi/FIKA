7dacaf33f902ca8e3359acd9c4951b9636bc06a6
Linus Berg <linus@oc6478442246.ibm.com>
2019-04-03 18:49:46 +0200

    React Boilerplate Code

---
+++ app/containers/Debug/index.js
@@ -0,0 +1,23 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a necessity for you then you can refactor it and remove
+ * the linting exception.
+ */
+
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class Debug extends React.PureComponent {
+  render() {
+    return (
+      <h1>
+        Debug
+      </h1>
+    );
+  }
+}
