fcb1aacdab304594aea4fd04e61156791d5777fc
bnjy <benrot@kth.se>
2019-04-30 12:10:51 +0200

    login module

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,7 +13,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import Debug from 'containers/Debug/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import LoginPage from '.components/login/LoginPage'
+import LoginPage from 'containers/Login/LoginPage'
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
@@ -22,7 +22,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/debug" component={Debug} />
-        <Route path="/logn" component={LoginPage} />
+        <Route path="/login" component={LoginPage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
