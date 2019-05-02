4ce4555b41ccad41699783a0f42e92e2dc71425f
bnjy <benrot@kth.se>
2019-05-02 13:00:44 +0200

    login

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../../components/css/Login.css";
 
+const bcrypt = require('bcrypt');
+
 class Login extends React.Component {
   render() {
     return (
@@ -12,12 +14,33 @@ class Login extends React.Component {
 }
 
 class LoginBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  submitLogin(e) {}
+  handleChange(e) {
+    let target = e.target;
+    let value = target.type === "checkbox" ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("The form was submitted with the following values:");
+    console.log(this.state);
+  }
 
   render() {
     return (
@@ -27,20 +50,28 @@ class LoginBox extends React.Component {
         <div className="box">
           <div className="input-group">
             <label htmlFor="username">Username</label>
-            <input type="username" name="username" placeholder="Username" />
+            <input
+              type="username"
+              name="username"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </div>
 
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" placeholder="Password" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
           </div>
         </div>
 
-        <button
-          type="button"
-          className="login-btn"
-          onClick={this.submitLogin.bind(this)}
-        >
+        <button type="button" className="login-btn" onClick={this.handleSubmit}>
           Login
         </button>
       </div>
