6e7c2f67190b5d8c153bfd073cbe6d92c1667f5c
Linus Berg <linus@oc6478442246.ibm.com>
2019-05-09 12:23:13 +0200

    Graphing

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,16 +10,11 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import Graph from '../../components/Graph/index';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
+    return <Graph id="g" />;
   }
 }
