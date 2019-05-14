f730bc0bc48586131ca6f384a1e57ac8cc77d445
bnjy <benrot@kth.se>
2019-05-14 15:02:50 +0200

    adding menu

---
+++ app/components/Dashboard/SimpleMenu.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+class SimpleMenu extends React.Component {
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+
+    return (
+      <div>
+        <Button
+          aria-owns={anchorEl ? 'simple-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          Show Dates
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem onClick={this.handleClose}>Date 1</MenuItem>
+          <MenuItem onClick={this.handleClose}>Date 2</MenuItem>
+          <MenuItem onClick={this.handleClose}>Date 3</MenuItem>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default SimpleMenu;
