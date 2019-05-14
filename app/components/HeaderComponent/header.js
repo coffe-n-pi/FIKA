14f32d6762099dcb2734905a3d73e47338ccaba7
Pebo Hamza <Pebo@n184-p236.eduroam.kth.se>
2019-05-14 15:07:12 +0200

    added old tests

--- app/components/HeaderComponent/header.js
+++ app/components/HeaderComponent/header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './header.css';
+
 const daftImg = require('./../../images/daft2.png');
 export default function Header() {
   return (
     <div>
-      <img className="logo" src={daftImg} alt="DAFT logo" />
+      <img className="logo" src={daftImg} alt="DAFT logo" test-data="logo" />
       <button className="logout" type="submit">
         Logout
       </button>
